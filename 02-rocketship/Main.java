4ff047826fc356950959efac387c9b7425a3eb9a
Bogdan Volobuev <70727437+Nervell@users.noreply.github.com>
2022-10-19 10:05:01 -0400

    Add files via upload

---
+++ 02-rocketship/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        Slash();
+        Box();
+        Sign();
+        Box();
+        Slash();
+    }
+    public static void Slash() {
+        System.out.println("   /\\  ");
+        System.out.println("  /  \\ ");
+        System.out.println(" /    \\ ");
+    }
+    public static void Box() {
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+    }
+
+    public static void  Sign(){
+        System.out.println("|United|");
+        System.out.println("|States|");
+    }
+
+    public static void Rocket() {
+        System.out.println("   /\\  ");
+        System.out.println("  /  \\ ");
+        System.out.println(" /    \\ ");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("|United|");
+        System.out.println("|States|");
+        System.out.println("+------+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+------+");
+        System.out.println("   /\\  ");
+        System.out.println("  /  \\ ");
+        System.out.println(" /    \\ ");
+    }
+
+}
